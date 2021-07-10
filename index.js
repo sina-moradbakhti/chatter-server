f79d0a940f6d6999ba07124d5dc24e587a5de8a4
sina moradbakhti <sina_moradbakhti@yahoo.com>
2021-07-10 19:14:51 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!")
+})
+
+io.on('connection', socket => {
+    //Get the chatID of the user and join in a room of the same chatID
+    chatID = socket.handshake.query.chatID
+    socket.join(chatID)
+
+    //Leave the room if the user closes the socket
+    socket.on('disconnect', () => {
+        socket.leave(chatID)
+    })
+
+    //Send message to only a particular user
+    socket.on('send_message', message => {
+        receiverChatID = message.receiverChatID
+        senderChatID = message.senderChatID
+        content = message.content
+
+        //Send message to only that particular room
+        socket.in(receiverChatID).emit('receive_message', {
+            'content': content,
+            'senderChatID': senderChatID,
+            'receiverChatID': receiverChatID,
+        })
+    })
+});
+
+const PORT = 6666;
+http.listen(PORT)
+console.log(`Server is listening on port ${PORT}`);
